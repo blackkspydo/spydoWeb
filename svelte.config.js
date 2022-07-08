d067cfd708977c41ba3f39725effab1b388c5d17
Kishor U <upadhyaya13kishor@gmail.com>
2022-07-08 20:44:03 +0545

    updated config for sass

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,13 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
-
+	preprocess: preprocess({
+		typescript: true,
+		scss: {
+			includePaths: ['./src/styles'],
+			outputStyle: 'compressed'
+		}
+	}),
 	kit: {
 		adapter: adapter()
 	}
