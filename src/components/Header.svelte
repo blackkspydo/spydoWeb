63821f7a4f2561e86faa3fdf4b0532eb494f8d55
Kishor U <upadhyaya13kishor@gmail.com>
2022-07-08 20:44:21 +0545

    added nav bar

---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Navigation from "./Navigation.svelte";
+</script>
+<div class="flex-container">
+    <h2>Blackk Spydo</h2>
+    <Navigation />
+</div>
+
+<style type="scss">
+    .flex-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        width: 90%;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,55 @@
+<script>
+    import {navigation} from "../store/data";
+</script>
+
+<nav>
+    <ul>
+        {#each navigation as item}
+            <li>
+                <a href={item.url}>{item.name}</a>
+            </li>
+        {/each}
+    </ul>
+</nav>
+
+<style
+type="scss"
+>
+@import "../scss/mixins.scss";
+@import "../scss/variables.scss";
+
+ul{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    li{
+        list-style: none;
+        a{
+            text-decoration: none;
+            color: $black;
+            font-size: 1.2em;
+            padding: toRem(10) toRem(5);
+            margin: toRem(10);
+            transition: $transition;
+            position: relative;
+            &::after{
+                content: "";
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 0;
+                height: toRem(3);
+                background: $black;
+                transition: $transition;
+                }
+            &:hover{
+               &::after{
+                   width: 100%;
+               }
+            }
+        }
+    }
+}
+
+</style>
