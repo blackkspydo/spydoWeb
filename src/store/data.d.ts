694c94c172f9d740f5920506e54d65b3d8756b1b
Kishor U <upadhyaya13kishor@gmail.com>
2022-07-08 20:43:35 +0545

    added data store

---
+++ src/store/data.d.ts
@@ -0,0 +1,6 @@
+
+export interface data {
+	name: string;
+	value: string | Array<string>;
+	url?: string;
+}
---
+++ src/store/data.ts
@@ -0,0 +1,113 @@
+import type {data } from './data.d';
+export const social: data[] = [
+	{
+		name: 'Github',
+		value: 'https://github.com/blackkspydo',
+		url: 'https://github.com/blackkspydo',
+			
+	},
+	{
+		name: 'Twitter',
+		value: 'https://twitter.com/_blackkspydo',
+		url: 'https://twitter.com/_blackkspydo',
+	},
+	{
+		name: 'Instagram',
+		value: 'https://instagram.com/blackkspydo',
+		url: 'https://instagram.com/blackkspydo/',
+	},
+	{
+		name: 'LinkedIn',
+		value: 'https://linkedin.com/in/blackkspydo',
+		url: 'https://linkedin.com/in/blackkspydo/',
+	},
+	{
+		name: 'Email',
+		value: 'work@blackkspydo.com',
+		url: 'mailto:work@blackkspydo.com',
+	}
+];
+
+export const personalInfo : data[] = [
+	{
+		name: 'pseudoname',
+		value: 'Blackk'
+	},
+
+	{
+		name: 'lastname',
+		value: 'Spydo'
+	},
+
+	{
+		name: 'age',
+		value: 'Little more than the age of universe'
+	},
+
+	{
+		name: 'gender',
+		value: 'male'
+	},
+	{
+		name: 'hobbies',
+		value: ['Breathing', 'Coding', 'Existing', 'Hiding dead bodies']
+	}
+];
+
+export const navigation = [
+	{
+		name: 'Home',
+		url: '/'
+	},
+	{
+		name: 'Contact',
+		url: 'mailto:work@blackkspydo.com'
+	},
+	{
+		name: 'Tinify.pro',
+		url: 'https://tinify.pro/'
+	},
+	{
+		name: 'Github',
+		url: 'https://github.com/blackkspydo'
+	}
+];
+
+export const skills: data[] = [
+	{
+		name: 'HTML',
+		value: 'Expert'
+	},
+	{
+		name: 'CSS',
+		value: 'Expert'
+	},
+	{
+		name: 'Typescript',
+		value: 'Intermediate'
+	},
+	{
+		name: 'ReactJs',
+		value: 'Intermediate'
+	},
+	{
+		name: 'NodeJs',
+		value: 'Intermediate'
+	},
+	{
+		name: 'git',
+		value: 'Intermediate'
+	},
+	{
+		name: 'Figma',
+		value: 'Intermediate'
+	},
+	{
+		name: 'Python',
+		value: 'Beginner'
+	},
+	{
+		name: 'Life',
+		value: 'Beginner'
+	}
+];
